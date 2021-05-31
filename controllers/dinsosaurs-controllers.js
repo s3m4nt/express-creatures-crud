940135ce54258be0202ee6c0af1e9bb05cbedb75
Brian Young <berkshirehunt@gmail.com>
2021-05-31 13:03:02 -0700

    controllers installed, pre H creatures functioning now

--- controllers/dinsosaurs-controllers.js
+++ controllers/dinsosaurs-controllers.js
@@ -1,21 +1,9 @@
-// Required packages
 const express = require('express')
-// rowdy logger for logging our routes
-rowdy = require('rowdy-logger')
+const router = express.Router()
+
 const fs = require('fs')
-const layouts = require('express-ejs-layouts')
-const methodOverride = require('method-override')
-// Config app
-const router = express()
-const rowdyResults = rowdy.begin(router)
-const PORT = 3000
-router.set('view engine', 'ejs')
-router.use(express.urlencoded({ extended: true }))
-// Site starts at /public ->
-router.use(express.static(__dirname + '/public'))
-router.use(layouts)
-// method override so we can put and delete
-router.use(methodOverride('_method'))
+
+// ////////////////////////////////////// DINOSAURS ->
 
 // Stub out my routes
 // GET /dinosaurs -- read all dinos
@@ -27,6 +15,80 @@ router.get('/dinosaurs', (req, res) => {
   console.log(dinoData)
   // send back the json to Postman
   res.render('dinosaurs/index.ejs', { dinoData: dinoData })
+})
+
+// POST /dinosaurs -- CREATE a new dino -- redirect to /dinosaurs
+router.post('/dinosaurs', (req, res) => {
+  // read dino file
+  const dinosaurs = fs.readFileSync('./dinosaurs.json')
+  const dinoData = JSON.parse(dinosaurs)
+
+  console.log(req.body)
+  // add data from the request body to the dino data
+  dinoData.push(req.body)
+
+  // write the file
+  fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+  // redirect to /dinosaurs
+  res.redirect('/dinosaurs')
+})
+
+// GET /dinosaurs/new -- READ (show) a form to edit one dino
+router.get('/dinosaurs/new', (req, res) => {
+  res.render('dinosaurs/new.ejs')
+})
+
+// GET /dinosaurs/:id -- READ one specific dino // GET /dinosaurs/new -- READ (show) a form to add a dino
+router.get('/dinosaurs/:id', (req, res) => {
+  // get our dino data
+  const dinosaurs = fs.readFileSync('./dinosaurs.json')
+  const dinoData = JSON.parse(dinosaurs)
+  // look up ine dino with the request parameters
+  const dino = dinoData[req.params.id]
+  // Send one dino back
+  res.json({ dino })
+})
+
+// GET /dinosaurs/edit/:id -- READ (show) form to edit one dino
+router.get('/dinosaurs/edit/:id', (req, res) => {
+  // get the dino info to populate the form
+  const dinosaurs = fs.readFileSync('./dinosaurs.json')
+  const dinoData = JSON.parse(dinosaurs)
+  const dino = dinoData[req.params.id]
+  console.log(dino)
+  // render the template
+  res.render('dinosaurs/edit.ejs', { dino: dino, dinoId: req.params.id })
+})
+
+// PUT /dinsosaurs/:id -- update (edit) one dino -- redirect to /dinosaurs
+router.put('/dinosaurs/:id', (req, res) => {
+  console.log(req.body)
+  // get dino data from our json
+  const dinosaurs = fs.readFileSync('./dinosaurs.json')
+  const dinoData = JSON.parse(dinosaurs)
+  // get dino data form the req.params.id and use the req.body to update
+  dinoData[req.params.id].name = req.body.name
+  dinoData[req.params.id].type = req.body.type
+
+  // write the json file
+  fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+  // redirect to /dinosaurs
+  res.redirect('/dinosaurs')
+})
+
+// DELETE /dinosaur/:id -- destroy one specific dino
+router.delete('/dinosaurs/:id', (req, res) => {
+  // get our dino json
+  const dinosaurs = fs.readFileSync('./dinosaurs.json')
+  const dinoData = JSON.parse(dinosaurs)
+  // remove one dino from the array
+  dinoData.splice(req.params.id, 1)
+  // save dinosaur .json
+  fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+  // redirect to /dinosaurs
+  res.redirect('/dinosaurs')
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require('express')
 // rowdy logger for logging our routes
 rowdy = require('rowdy-logger')
-const fs = require('fs')
+// const fs = require('fs')
+// makes layouts the default BASE doc
 const layouts = require('express-ejs-layouts')
 const methodOverride = require('method-override')
 // Config app
@@ -23,105 +24,10 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-// Stub out my routes
-// GET /dinosaurs -- read all dinos
-app.get('/dinosaurs', (req, res) => {
-  // read the dinosaurs.json
-  const dinosaurs = fs.readFileSync('./dinosaurs.json')
-  // parsed the json buffer to clean it up! -
-  const dinoData = JSON.parse(dinosaurs)
-  console.log(dinoData)
-  // send back the json to Postman
-  res.render('dinosaurs/index.ejs', { dinoData: dinoData })
-})
+// requiring controllers
+app.use('/', require('./controllers/prehistoric_creatures-controllers'))
+app.use('/', require('./controllers/dinsosaurs-controllers'))
 
-// GET /prehistoric_creatures -- read all dinos
-app.get('/prehistoric_creatures', (req, res) => {
-  // read the dinosaurs.json
-  const prehistoric_creatures = fs.readFileSync('./prehistoric_creatures.json')
-  // parsed the json buffer to clean it up! -
-  const prehistoric_creaturesData = JSON.parse(prehistoric_creatures)
-  console.log(prehistoric_creaturesData)
-  // send back the json to Postman
-  res.render('prehistoric_creatures/index.ejs', {
-    prehistoric_creaturesData: prehistoric_creaturesData,
-  })
-})
-
-// POST /dinosaurs -- CREATE a new dino -- redirect to /dinosaurs
-app.post('/dinosaurs', (req, res) => {
-  // read dino file
-  const dinosaurs = fs.readFileSync('./dinosaurs.json')
-  const dinoData = JSON.parse(dinosaurs)
-
-  console.log(req.body)
-  // add data from the request body to the dino data
-  dinoData.push(req.body)
-
-  // write the file
-  fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
-
-  // redirect to /dinosaurs
-  res.redirect('/dinosaurs')
-})
-
-// GET /dinosaurs/new -- READ (show) a form to edit one dino
-app.get('/dinosaurs/new', (req, res) => {
-  res.render('dinosaurs/new.ejs')
-})
-
-// GET /dinosaurs/:id -- READ one specific dino // GET /dinosaurs/new -- READ (show) a form to add a dino
-app.get('/dinosaurs/:id', (req, res) => {
-  // get our dino data
-  const dinosaurs = fs.readFileSync('./dinosaurs.json')
-  const dinoData = JSON.parse(dinosaurs)
-  // look up ine dino with the request parameters
-  const dino = dinoData[req.params.id]
-  // Send one dino back
-  res.json({ dino })
-})
-
-// GET /dinosaurs/edit/:id -- READ (show) form to edit one dino
-app.get('/dinosaurs/edit/:id', (req, res) => {
-  // get the dino info to populate the form
-  const dinosaurs = fs.readFileSync('./dinosaurs.json')
-  const dinoData = JSON.parse(dinosaurs)
-  const dino = dinoData[req.params.id]
-  console.log(dino)
-  // render the template
-  res.render('dinosaurs/edit.ejs', { dino: dino, dinoId: req.params.id })
-})
-
-// PUT /dinsosaurs/:id -- update (edit) one dino -- redirect to /dinosaurs
-app.put('/dinosaurs/:id', (req, res) => {
-  console.log(req.body)
-  // get dino data from our json
-  const dinosaurs = fs.readFileSync('./dinosaurs.json')
-  const dinoData = JSON.parse(dinosaurs)
-  // get dino data form the req.params.id and use the req.body to update
-  dinoData[req.params.id].name = req.body.name
-  dinoData[req.params.id].type = req.body.type
-
-  // write the json file
-  fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
-
-  // redirect to /dinosaurs
-  res.redirect('/dinosaurs')
-})
-
-// DELETE /dinosaur/:id -- destroy one specific dino
-app.delete('/dinosaurs/:id', (req, res) => {
-  // get our dino json
-  const dinosaurs = fs.readFileSync('./dinosaurs.json')
-  const dinoData = JSON.parse(dinosaurs)
-  // remove one dino from the array
-  dinoData.splice(req.params.id, 1)
-  // save dinosaur .json
-  fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
-  // redirect to /dinosaurs
-  res.redirect('/dinosaurs')
-})
-//
 // List on port
 app.listen(PORT, () => {
   rowdyResults.print()
